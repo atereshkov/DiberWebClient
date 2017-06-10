ddb704fe756e63538164c41687fe1912a610bc6b
Alexander Tereshkov <ter.alexanderandr@gmail.com>
2017-06-10 23:02:38 +0300

    Handle PathLocationStrategy Routing

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const path = require('path');
 // Run the app by serving the static files in the dist directory
 app.use(express.static(path.join(__dirname, 'dist')));
 
+// For all GET requests, send back index.html so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 // Start the app by listening on the default Heroku port
 
 app.listen(process.env.PORT || 8080);
