ea672cc244fb6d066f20f41315b23f1a43015632
Alexander Tereshkov <ter.alexanderandr@gmail.com>
2017-06-22 00:45:50 +0300

    Implemented LoggedIn guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
 import { MainPageComponent } from "./components/main-page/main-page.component";
 import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
-import {AuthComponent} from "./components/auth/auth.component";
+import { AuthComponent } from "./components/auth/auth.component";
+import { LoggedInGuard } from "./guards/logged-in.guard"
 
 export const ROUTES: Routes = [
   //{ path: '', redirectTo: '/main-page', pathMatch: 'full' },
   { path: '', component: MainPageComponent },
-  { path: 'signin', component: AuthComponent },
+  { path: 'signin', component: AuthComponent, canActivate: [LoggedInGuard] },
   { path: '**', component: PageNotFoundComponent }
 ];
 
---
+++ src/app/guards/logged-in.guard.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from "../services/auth.service";
+
+@Injectable()
+export class LoggedInGuard implements CanActivate {
+
+  constructor(private router: Router, private authService: AuthService) {
+
+  }
+
+  canActivate(): boolean {
+    return !this.authService.isUserLoggedIn();
+  }
+
+}
