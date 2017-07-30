9951b22ec52ce3c2058d170481d57cc25b15279c
Alexander Tereshkov <ter.alexanderandr@gmail.com>
2017-07-30 18:22:38 +0300

    Hide hider for admin dashboard. Disabled for now. Added link to the header for admin route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  hideHeaderWhen: Array<string> = ['/admin'];
+
+  constructor(private _router: Router) {
+  }
+
+  ngOnInit() {
+  }
+
+  shouldShowHeader() {
+    return !(this.hideHeaderWhen.indexOf(this._router.url) > -1);
+  }
+
 }
