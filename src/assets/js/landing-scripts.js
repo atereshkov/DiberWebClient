72e6d0db58faa037c0d694cfb0b96b98afc5a591
Alexander Tereshkov <ter.alexanderandr@gmail.com>
2017-08-15 22:07:51 +0300

    Fixed ambiguous loading of bootstrap.min.js and jquery.min.js

--- src/assets/js/landing-scripts.js
+++ src/assets/js/landing-scripts.js
@@ -1,28 +1,35 @@
 (function($) {
-    "use strict";
+  "use strict";
 
-    $('body').scrollspy({
-        target: '.fixed-top',
-        offset: 60
-    });
+  $('body').scrollspy({
+    target: '.fixed-top',
+    offset: 70
+  });
 
-    new WOW().init();
-    
-    $('a.page-scroll').bind('click', function(event) {
-        var $ele = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($ele.attr('href')).offset().top - 60)
-        }, 1450, 'easeInOutExpo');
-        event.preventDefault();
-    });
-    
-    $('#collapsingNavbar li a').click(function() {
-        /* always close responsive nav after click */
-        $('.navbar-toggler:visible').click();
-    });
+  new WOW().init();
 
-    $('#galleryModal').on('show.bs.modal', function (e) {
-       $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
-    });
+  $('a.page-scroll').bind('click', function(event) {
+    var $ele = $(this);
+    var href = $ele.attr('href');
+    if (href.indexOf('#')!=-1){
+      var hash = href.substring(href.indexOf('#'),href.length);
+      console.log(hash);
+      $('html, body').stop().animate({
+        scrollTop: ($(hash).offset().top - 60)
+      }, 1450, 'easeInOutExpo');
+      event.preventDefault();
+    }
+  });
+
+  $('#collapsingNavbar li a').click(function() {
+    /* always close responsive nav after click */
+    $('.navbar-toggler:visible').click();
+  });
+
+  $('#galleryModal').on('show.bs.modal', function (e) {
+    $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
+  });
+
+  $('[data-toggle=popover]').popover();
 
 })(jQuery);
