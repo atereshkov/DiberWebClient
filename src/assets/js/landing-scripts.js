43377b10b8cfbcbb476a1e78b6180d87149014f2
Alexander Tereshkov <ter.alexanderandr@gmail.com>
2017-09-10 19:22:56 +0300

    "Scroll To Top" button for landing page.

--- src/assets/js/landing-scripts.js
+++ src/assets/js/landing-scripts.js
@@ -1,4 +1,4 @@
-(function($) {
+(function ($) {
   "use strict";
 
   $('body').scrollspy({
@@ -8,11 +8,11 @@
 
   new WOW().init();
 
-  $('a.page-scroll').bind('click', function(event) {
+  $('a.page-scroll').bind('click', function (event) {
     var $ele = $(this);
     var href = $ele.attr('href');
-    if (href.indexOf('#')!=-1){
-      var hash = href.substring(href.indexOf('#'),href.length);
+    if (href.indexOf('#') != -1) {
+      var hash = href.substring(href.indexOf('#'), href.length);
       console.log(hash);
       $('html, body').stop().animate({
         scrollTop: ($(hash).offset().top - 60)
@@ -21,15 +21,37 @@
     }
   });
 
-  $('#collapsingNavbar li a').click(function() {
+  $('#collapsingNavbar li a').click(function () {
     /* always close responsive nav after click */
     $('.navbar-toggler:visible').click();
   });
 
   $('#galleryModal').on('show.bs.modal', function (e) {
-    $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
+    $('#galleryImage').attr("src", $(e.relatedTarget).data("src"));
   });
 
   $('[data-toggle=popover]').popover();
+
+  // Scroll to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 500) {
+      $('#back-to-top')
+        .fadeIn(300);
+    } else {
+      $('#back-to-top')
+        .fadeOut(300);
+    }
+  });
+
+  // Scroll body to 0px on click
+  $('#back-to-top').click(function () {
+    $('#back-to-top').tooltip('hide');
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
+
+  $('#back-to-top').tooltip('show');
 
 })(jQuery);
