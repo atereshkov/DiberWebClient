39c9a241533c61afeacfc341248ba2b748c841e9
Alexander Tereshkov <ter.alexanderandr@gmail.com>
2017-09-17 18:36:08 +0300

    Dashboard, page not found, profile, settings modules extracted.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,45 +4,45 @@ import {HttpModule, RequestOptions, XHRBackend, Http} from "@angular/http";
 import {AppRoutingModule} from "./app-routing.module";
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {AppComponent} from "./app.component";
-import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
-import {CourierDashboardComponent} from "./components/dashboard/courier-dashboard/courier-dashboard.component";
-import {ClientDashboardComponent} from "./components/dashboard/client-dashboard/client-dashboard.component";
 import {NotAuthorizedGuard} from "./guards/not-authorized.guard";
 import {AuthService} from "./services/auth.service";
 import {AdminGuard} from "./guards/admin.guard";
 import {OrderService} from "./services/order.service";
-import {DashboardComponent} from "./components/dashboard/dashboard.component";
 import {httpFactory} from "./services/interceptors/http.factory";
 import {Router} from "@angular/router";
 import {ClientGuard} from "./guards/client.guard";
 import {CourierGuard} from "./guards/courier.guard";
 import {LoggedInGuard} from "./guards/logged-in.guard";
-import {ProfileComponent} from "./components/profile/profile.component";
-import {SettingsComponent} from "./components/settings/settings.component";
 import {AdminPanelModule} from "./components/admin-panel/admin-panel.module";
 import {LandingPageModule} from "./components/landing-page/landing-page.module";
 import {LandingHeaderComponent} from "./components/landing-page/header/header.component";
 import {LandingFooterComponent} from "./components/landing-page/footer/footer.component";
 import {RegisterModule} from "./components/register/register.module";
 import {AuthModule} from "./components/auth/auth.module";
+import {CourierDashboardModule} from "./components/dashboard/courier-dashboard/courier-dashboard.module";
+import {ClientDashboardModule} from "./components/dashboard/client-dashboard/client-dashboard.module";
+import {PageNotFoundModule} from "./components/page-not-found/page-not-found.module";
+import {ProfileModule} from "./components/profile/profile.module";
+import {SettingsModule} from "./components/settings/settings.module";
+import {DashboardModule} from "./components/dashboard/dashboard.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageNotFoundComponent,
-    CourierDashboardComponent,
-    ClientDashboardComponent,
     LandingFooterComponent,
-    LandingHeaderComponent,
-    DashboardComponent,
-    ProfileComponent,
-    SettingsComponent
+    LandingHeaderComponent
   ],
   imports: [
     NgbModule.forRoot(),
     BrowserModule,
     HttpModule,
     LandingPageModule,
+    DashboardModule,
+    ProfileModule,
+    SettingsModule,
+    CourierDashboardModule,
+    ClientDashboardModule,
+    PageNotFoundModule,
     RegisterModule,
     AuthModule,
     AdminPanelModule,
---
+++ src/app/components/dashboard/client-dashboard/client-dashboard.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {ClientDashboardComponent} from "./client-dashboard.component";
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    ClientDashboardComponent
+  ]
+})
+
+export class ClientDashboardModule {
+
+}
