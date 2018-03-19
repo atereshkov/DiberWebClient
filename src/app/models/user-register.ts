b78a46fecaf1c27f2dafdf1897da97e3b0969494
Alexander Tereshkov <alexander.tereshkov@exposit.com>
2018-03-19 23:49:58 +0300

    Fixed broken property of UserRegister

--- src/app/models/user-register.ts
+++ src/app/models/user-register.ts
@@ -6,14 +6,16 @@ export class UserRegister {
   fullname: string;
   customer: boolean;
   courier: boolean;
+  enabled: boolean;
 
-  constructor(username: string, password: string, email: string, fullname: string, customer: boolean, courier: boolean) {
+  constructor(username: string, password: string, email: string, fullname: string, customer: boolean, courier: boolean, enabled: boolean) {
     this.username = username;
     this.password = password;
     this.email = email;
     this.fullname = fullname;
     this.customer = customer;
     this.courier = courier;
+    this.enabled = enabled;
   }
 
 }
