050534f23b0c4db7d08304eb00757bb245166417
Alexander Tereshkov <alexander.tereshkov@exposit.com>
2018-04-07 17:18:33 +0300

    Order details request

--- src/app/components/dashboard/client-dashboard/content/client-order-detaills/client-order-details.component.ts
+++ src/app/components/dashboard/client-dashboard/content/client-order-detaills/client-order-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Order} from '../../../../../models/order';
+import {ActivatedRoute} from '@angular/router';
+import {OrderService} from '../../../../../services/order.service';
 
 @Component({
   selector: 'app-client-order-details',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientOrderDetailsComponent implements OnInit {
 
-  constructor() { }
+  public order: Order;
+  private id: number;
+  public loading = false;
+
+  constructor(private orderService: OrderService, private router: ActivatedRoute) {
+    this.id = parseInt(this.router.snapshot.paramMap.get('id'));
+  }
 
   ngOnInit() {
+    this.orderService.getOrder(this.id)
+      .subscribe((order: Order) => {
+        this.order = order;
+        this.loadOrderDetails();
+      });
+  }
+
+  private loadOrderDetails() {
+    // TODO complete UI with this.order
   }
 
 }
