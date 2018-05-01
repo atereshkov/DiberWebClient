9d5ed38e96fcaec3855ce7beecb9076fef5209cf
Alexander Tereshkov <alexander.tereshkov@exposit.com>
2018-05-01 17:55:08 +0300

    Profile request

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../models/user';
+import {UserAuthority} from '../../helper/user.authority';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  public user: User;
+  public loading = false;
+  public role = '';
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.loadProfileData();
+  }
+
+  private loadProfileData() {
+    const currentUser: User = UserAuthority.getCurrentUser();
+
+    if (UserAuthority.isCustomer()) {
+      this.role = 'Customer';
+    } else if (UserAuthority.isCourier()) {
+      this.role = 'Courier';
+    } else if (UserAuthority.isAdmin()) {
+      this.role = 'Admin';
+    }
+
+    this.loading = true;
+    this.userService.getUser(currentUser.id)
+      .subscribe((user: User) => {
+        this.loading = false;
+        this.user = user;
+      });
   }
 
 }
